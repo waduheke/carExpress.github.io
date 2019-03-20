3437b10d1f77e590138eaf915407b3170aca1e21
waduheke <38802566+waduheke@users.noreply.github.com>
2019-03-20 14:47:18 +0800

    打包文件

---
+++ build/precache-manifest.4dba2b726483cad709ce22d29e08c914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "736d9b84df04404768ad",
+    "url": "/static/js/main.7bbe6e03.chunk.js"
+  },
+  {
+    "revision": "506fb84ccf30ddc100c8",
+    "url": "/static/js/2.400e862f.chunk.js"
+  },
+  {
+    "revision": "736d9b84df04404768ad",
+    "url": "/static/css/main.6d4dfde5.chunk.css"
+  },
+  {
+    "revision": "506fb84ccf30ddc100c8",
+    "url": "/static/css/2.03d153b4.chunk.css"
+  },
+  {
+    "revision": "0c83c8baebe299937f600d266e51b0a3",
+    "url": "/index.html"
+  }
+];
