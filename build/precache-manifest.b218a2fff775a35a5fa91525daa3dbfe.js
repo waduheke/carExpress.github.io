491997ff409cf277542638aace09581a8121706c
waduheke <38802566+waduheke@users.noreply.github.com>
2019-03-20 16:57:56 +0800

    Add files via upload
    更新地图

---
+++ build/precache-manifest.b218a2fff775a35a5fa91525daa3dbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "20a5ab5f5dd8c4a2fcdf",
+    "url": "/static/js/main.4ba5ad39.chunk.js"
+  },
+  {
+    "revision": "506fb84ccf30ddc100c8",
+    "url": "/static/js/2.400e862f.chunk.js"
+  },
+  {
+    "revision": "20a5ab5f5dd8c4a2fcdf",
+    "url": "/static/css/main.6d4dfde5.chunk.css"
+  },
+  {
+    "revision": "506fb84ccf30ddc100c8",
+    "url": "/static/css/2.03d153b4.chunk.css"
+  },
+  {
+    "revision": "f8a2c8dbbae5c52079934fb532f950b9",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dba2b726483cad709ce22d29e08c914.js"
+  "/precache-manifest.b218a2fff775a35a5fa91525daa3dbfe.js"
 );
 
 workbox.clientsClaim();
